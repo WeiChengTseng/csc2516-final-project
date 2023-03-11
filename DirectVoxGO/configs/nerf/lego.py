949d0823858be28e0c1c21195b1ba6c1b3fd199a
Wei-Cheng Tseng <ethanweichengtseng@gmail.com>
2023-03-11 14:49:51 -0500

    update directvoxgo

--- DirectVoxGO/configs/nerf/lego.py
+++ DirectVoxGO/configs/nerf/lego.py
@@ -4,7 +4,8 @@ expname = 'dvgo_lego'
 basedir = './logs/nerf_synthetic'
 
 data = dict(
-    datadir='./data/nerf_synthetic/lego',
+    # datadir='./data/nerf_synthetic/lego',
+    datadir='../nerf/data/nerf_synthetic/lego',
     dataset_type='blender',
     white_bkgd=True,
 )
